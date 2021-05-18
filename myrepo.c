eadb28531220e4fceca668c127814f4f3559bc4d
CharithSathsara <charithsathsara169@gmail.com>
2021-05-18 18:59:23 +0530

    update 4 lines

--- myrepo.c
+++ myrepo.c
@@ -2,6 +2,10 @@
 
 int main(){
 	
+	printf("my mother name is Sudeni");
+	printf("my mother name is Chaminda");
 	printf("my mother name is Waruni");
+	printf("my mother name is Charith");
+	
 	return 0;
 }
